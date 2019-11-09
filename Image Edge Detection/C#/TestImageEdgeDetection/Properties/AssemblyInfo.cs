7a532c421e3834653758952e375627b56e435a64
Borganov <berclazalexandre@gmail.com>
2019-11-09 10:43:55 +0100

    Add Test Project

---
+++ Image Edge Detection/C#/TestImageEdgeDetection/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestImageEdgeDetection")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestImageEdgeDetection")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("823b1649-dd24-46d8-8afe-0f8c46db9fe3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Image Edge Detection/C#/TestImageEdgeDetection/TestMainForm.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestImageEdgeDetection
+{
+    [TestClass]
+    public class TestMainForm
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
